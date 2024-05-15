6ecabe63d802321c3fe959f0206c7e6bea6aa195
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2024-05-16 01:31:51 +0900

    Video input type and video_tag is used instead of video-url input type

--- assets/image-with-text.js
+++ assets/image-with-text.js
@@ -1,26 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const lazyVideos = [...document.querySelectorAll('video.lazy')];
-
-  if ('IntersectionObserver' in window) {
-    const lazyVideoObserver = new IntersectionObserver(function (entries, observer) {
-      entries.forEach(function (video) {
-        if (video.isIntersecting) {
-          for (let source in video.target.children) {
-            let videoSource = video.target.children[source];
-            if (typeof videoSource.tagName === 'string' && videoSource.tagName === 'SOURCE') {
-              videoSource.src = videoSource.dataset.src;
-            }
-          }
-
-          video.target.load();
-          video.target.classList.remove('lazy');
-          lazyVideoObserver.unobserve(video.target);
-        }
-      });
+  const lazyVideo = document.getElementById('lazy-video');
+  const observer = new IntersectionObserver(function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const posterPath = entry.target.getAttribute('data-poster');
+        entry.target.poster = posterPath;
+        observer.unobserve(entry.target);
+      }
     });
+  });
 
-    lazyVideos.forEach(function (lazyVideo) {
-      lazyVideoObserver.observe(lazyVideo);
-    });
-  }
+  observer.observe(lazyVideo);
 });
